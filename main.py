0e5d0cb24aee411411785b1992108978f6363e89
TorChrEriksen <trofast@gmail.com>
2019-11-20 17:27:26 +0100

    Rename imports to make it easy to see where in the code it is used.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as TK
+import tkinter.ttk as TTK
 from functools import partial
 from conwindow import spawnwindow
 
@@ -8,43 +8,42 @@ from conwindow import spawnwindow
 def program_exit(window):
     window.destroy()
 
-
 # Create the root element and set a title for the window
-root = Tk()
+root = TK.Tk()
 root.title('A-Series connection tool')
 
 # Normal setup stuff to make sure that we have a frame to work in and some
 # half-way decent styling.
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe = TTK.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(TK.N, TK.W, TK.E, TK.S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 # Create our two values to hold the ip and the port number:
-printer_ip = StringVar()
-printer_port = StringVar()
+printer_ip = TK.StringVar()
+printer_port = TK.StringVar()
 
 # Define default values
 printer_ip.set('127.0.0.1')
 printer_port.set('700')
 
 # Create two text labels
-ttk.Label(mainframe, text="ip address:").grid(column=0, row=0, sticky=W)
-ttk.Label(mainframe, text="port:").grid(column=1, row=0, sticky=W)
+TTK.Label(mainframe, text="ip address:").grid(column=0, row=0, sticky=TK.W)
+TTK.Label(mainframe, text="port:").grid(column=1, row=0, sticky=TK.W)
 
 # Create two elements to enter the IP and PORT into:
-ip_entry = ttk.Entry(mainframe, width=15, textvariable=printer_ip)
-ip_entry.grid(column=0, row=1, sticky=(W, E))
+ip_entry = TTK.Entry(mainframe, width=15, textvariable=printer_ip)
+ip_entry.grid(column=0, row=1, sticky=(TK.W, TK.E))
 
-port_entry = ttk.Entry(mainframe, width=6, textvariable=printer_port)
-port_entry.grid(column=1, row=1, sticky=(W, E))
+port_entry = TTK.Entry(mainframe, width=6, textvariable=printer_port)
+port_entry.grid(column=1, row=1, sticky=(TK.W, TK.E))
 
 # Create the connect button, which spawns a new window
-ttk.Button(mainframe, text="Connect", command=partial(
-    spawnwindow, root, printer_ip, printer_port)).grid(column=0, row=2, sticky=W)
+TTK.Button(mainframe, text="Connect", command=partial(
+    spawnwindow, root, printer_ip, printer_port)).grid(column=0, row=2, sticky=TK.W)
 
-ttk.Button(mainframe, text="Exit", command=partial(
-    program_exit, root)).grid(column=1, row=2, sticky=W)
+TTK.Button(mainframe, text="Exit", command=partial(
+    program_exit, root)).grid(column=1, row=2, sticky=TK.W)
 
 # Give every child item some padding so that it doesn't look like shit.
 for child in mainframe.winfo_children():
